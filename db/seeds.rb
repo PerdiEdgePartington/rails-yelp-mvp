50b788755721bd3c3c61e422effb90adfae85c9d
Perdita Edge-Partington <perdita.edge.partington@gmail.com>
2022-11-24 07:56:25 +0000

    second task so far

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+
+mildreds = { name: "Mildreds", address: "Soho", phone_number: "12345", category: "italian" }
+holy_carrot = { name: "Holy Carrot", address: "Knightsbridge", phone_number: "12345", category: "japanese" }
+spicebox = { name: "Spicebox", address: "Leytonstone", phone_number: "12345", category: "chinese" }
+power_plant = { name: "Powerplant", address: "Camden", phone_number: "12345", category: "belgian" }
+black_cat_cafe = { name: "Black Cat Cafe", address: "Cambridge", phone_number: "12345", category: "french" }
+
+[mildreds, holy_carrot, spicebox, power_plant, black_cat_cafe].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
